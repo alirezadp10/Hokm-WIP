97c1b07f8d8f0fbb50cd71623c063d3d70b60958
alirezadp10 <alirezadp10@gmail.com>
2024-10-29 17:14:46 +0330

    add docker

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.23.2
 
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/redis/rueidis v1.0.48 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
+	"github.com/redis/rueidis"
 	"gopkg.in/telebot.v4"
 	"log"
 	"os"
@@ -12,9 +14,9 @@ func main() {
 	bot, err := telebot.NewBot(telebot.Settings{
 		Token: os.Getenv("TELEGRAM_BOT_TOKEN"),
 		Poller: &telebot.Webhook{
-			Listen: "0.0.0.0:8443", // Address to listen for incoming webhook requests
+			Listen: "0.0.0.0:80", // Address to listen for incoming webhook requests
 			Endpoint: &telebot.WebhookEndpoint{
-				PublicURL: "https://8bea-46-100-55-166.ngrok-free.app", // Replace with your actual public URL
+				PublicURL: "https://8bb2-194-107-126-16.ngrok-free.app", // Replace with your actual public URL
 			},
 		},
 	})
@@ -24,6 +26,9 @@ func main() {
 
 	// Define a command handler
 	bot.Handle("/start", func(c telebot.Context) error {
+		client, _ := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{"redis:6379"}})
+		defer client.Close()
+		client.Do(context.Background(), client.B().Set().Key("key").Value("val").Nx().Build())
 		return c.Send("Webhook is now active!")
 	})
 
