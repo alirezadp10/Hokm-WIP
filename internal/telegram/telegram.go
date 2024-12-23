1c69ee195387287c3edb500026ae638d1d61cb63
alirezadp10 <alirezadp10@gmail.com>
2024-12-23 17:18:09 +0330

    temp

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -46,7 +46,7 @@ func startHandler(c telebot.Context, db *gorm.DB) error {
     return c.Send("Let's Play", &telebot.ReplyMarkup{
         InlineKeyboard: [][]telebot.InlineButton{{{
             Text:   trans.Get("start the game"),
-            WebApp: &telebot.WebApp{URL: os.Getenv("APP_URL") + "?username=" + url.QueryEscape(encrypted)},
+            WebApp: &telebot.WebApp{URL: os.Getenv("APP_URL") + "?user_id=" + url.QueryEscape(encrypted)},
         }}},
     })
 }
